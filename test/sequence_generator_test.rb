c92e6dc2d28fdb69cec22188bcf2c1cfdf22488d
Sara Simon <smbsimon@gmail.com>
2014-08-05 18:06:56 -0600

    Refactor test for clarity

--- test/sequence_generator_test.rb
+++ test/sequence_generator_test.rb
@@ -8,15 +8,25 @@ class SequenceGeneratorTest < Minitest::Test
   end
 
   def test_it_has_a_pool
-    generator = SequenceGenerator.new(%w[a b c d])
-    assert_equal %w[a b c d], generator.pool
+    possible_values = %w[r b g y]
+    generator = SequenceGenerator.new(possible_values)
+    assert_equal %w[r b g y], generator.pool
   end
 
-  def test_it_can_create_a_sequence
-    generator   = SequenceGenerator.new(%w[a b c d])
+  def test_it_can_create_a_sequence_of_a_given_length
+    possible_values = %w[r b g y]
+    generator = SequenceGenerator.new(possible_values)
     secret_code = generator.create(4)
 
     assert_equal 4, secret_code.size
+  end
+
+  def test_it_returns_an_array
+    possible_values = %w[r b g y]
+    generator = SequenceGenerator.new(possible_values)
+    secret_code = generator.create(4)
+
     assert_equal Array, secret_code.class
   end
+
 end
