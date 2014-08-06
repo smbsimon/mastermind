0a00debbb54e5ac9c7cc5ccdc05d5c28904e4e8a
Sara Simon <smbsimon@gmail.com>
2014-08-05 18:35:22 -0600

    Moved matcher tests into matcher_test class.

--- test/sequence_generator_test.rb
+++ test/sequence_generator_test.rb
@@ -3,30 +3,48 @@ require "minitest/pride"
 require_relative "../lib/sequence_generator"
 
 class SequenceGeneratorTest < Minitest::Test
+  def setup
+    @possible_values = %w[r b g y]
+    @generator = SequenceGenerator.new(@possible_values)
+  end
+
   def test_it_exits
     assert SequenceGenerator
   end
 
   def test_it_has_a_pool
-    possible_values = %w[r b g y]
-    generator = SequenceGenerator.new(possible_values)
-    assert_equal %w[r b g y], generator.pool
+    assert_equal %w[r b g y], @generator.pool
   end
 
   def test_it_can_create_a_sequence_of_a_given_length
-    possible_values = %w[r b g y]
-    generator = SequenceGenerator.new(possible_values)
-    secret_code = generator.create(4)
+    secret_code = @generator.create(4)
 
     assert_equal 4, secret_code.size
   end
 
   def test_it_returns_an_array
-    possible_values = %w[r b g y]
-    generator = SequenceGenerator.new(possible_values)
-    secret_code = generator.create(4)
+    secret_code = @generator.create(4)
 
     assert_equal Array, secret_code.class
   end
+
+  def test_it_generates_a_secret_code_with_only_letters_from_the_pool
+    secret_code = @generator.create(4)
+
+    assert secret_code.all? { |letter| @possible_values.include? letter }
+  end
+
+
+  #
+  # def test_it_generates_sequences_that_are_actually_random
+  #   10.times do
+  #     base_result = random_sequence
+  #     new_result = sequence.generate_sequence(["r", "b", "g", "y"])
+  #     refute_equal base_result, new_result
+  #   end
+  # end
+
+
+
 
 end
